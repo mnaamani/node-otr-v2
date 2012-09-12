a050053d1d3e63d3ab32e9861faa4a7539a5c5c3
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2012-09-12 16:38:11 +0300

    test strictly for POLICY.ALWAYS

--- examples/test.js
+++ examples/test.js
@@ -9,11 +9,11 @@ var alice = new libotr.User({name:'alice',keys:'./db/alice.keys',fingerprints:'.
 var BOB = alice.ConnContext("alice@telechat.org","telechat","BOB");
 
 //setup an OTR communication channel with Bob
-var otrchan_a = new libotr.OTRChannel(alice, BOB,{policy:libotr.POLICY("DEFAULT"),secret:'s3cr37'});
+var otrchan_a = new libotr.OTRChannel(alice, BOB,{policy:libotr.POLICY("ALWAYS"),secret:'s3cr37'});
 
 var bob = new libotr.User({name:'bob',keys:'./db/bob.keys',fingerprints:'./db/bob.fp'});
 var ALICE = bob.ConnContext("bob@telechat.org","telechat","ALICE");
-var otrchan_b = new libotr.OTRChannel(bob, ALICE,{secret:'s3cr37'});
+var otrchan_b = new libotr.OTRChannel(bob, ALICE,{policy:libotr.POLICY("ALWAYS"),secret:'s3cr37'});
 
 console.log("Alice's OTR fingerprint:",alice.state.fingerprint("alice@telechat.org","telechat"));
 console.log("Bob's OTR fingerprint:",bob.state.fingerprint("bob@telechat.org","telechat"));
@@ -37,20 +37,22 @@ otrchan_b.on("inject_message",function(msg){
 otrchan_a.on("message",function(msg){
     if(this.isEncrypted()) {
         console.log('encrypted: Bob->Alice: ', msg);
+        if(TEST_PASSED) exit_test("");
     }else{
-        console.log('not-encrypted: Bob->Alice: ',msg);
+        //policy is set to ALWAYS so we should not get any unencrypted messages!
+        console.log('not-encrypted!!!: Bob->Alice: ',msg);
     }
-    if(TEST_PASSED){ console.log("TEST PASSED"); process.exit();} 
 });
 
 //output incoming messages to console
 otrchan_b.on("message",function(msg){
     if(this.isEncrypted()) {
         console.log('encrypted: Alice->Bob: ', msg);
+        if(TEST_PASSED) exit_test("");
     }else{
-        console.log('not-encrypted: Alice->Bob: ',msg);
+        //policy is set to ALWAYS so we should not get any unencrypted messages!
+        console.log('not-encrypted!!!: Alice->Bob: ',msg);
     }
-    if(TEST_PASSED){ console.log("TEST PASSED"); process.exit();} 
 });
 
 //connection is encrypted..
@@ -67,7 +69,7 @@ otrchan_b.on("smp_request",function(){
 });
 
 //alice sends a message to bob
-otrchan_a.send("Hello, World!"); //this would not be encrypted.
+otrchan_a.send("Hello, World!");
 
 var TEST_PASSED=false;
 
@@ -75,12 +77,32 @@ var loop = setInterval(function(){
     if(otrchan_a.isEncrypted() && otrchan_a.isAuthenticated()){
         console.log("Finger print verification successful");    
         TEST_PASSED=true;
-        clearInterval(loop);
+        if(loop) clearInterval(loop);
         otrchan_b.send("Meet me at midnight...near the docks...");        
     }
 },500);
 
 var test_timeout = setTimeout(function(){
-    if(TEST_PASSED){ console.log("TEST PASSED"); } else { console.log("TEST FAILED"); }
     if(loop) clearInterval(loop);
+    exit_test("Timeout! - try again..");//usually something went wrong during SMP authentication
 },10000);
+
+function exit_test(msg){
+    dumpConnContext(otrchan_a,"Alice's ConnContext:");
+    dumpConnContext(otrchan_b,"Bob's ConnContext:");
+    console.log(msg);
+    if(TEST_PASSED){ console.log("== TEST PASSED ==\n"); } else { console.log("== TEST FAILED ==\n"); }
+    process.exit();
+}
+
+function dumpConnContext(chan,msg){
+    console.log(msg,
+        chan.context.username,
+        chan.context.protocol,
+        chan.context.accountname,
+        chan.context.fingerprint,
+        chan.context.protocol_version,
+        chan.context.msgstate,
+        chan.context.trust,
+        chan.context.smstate);
+}
