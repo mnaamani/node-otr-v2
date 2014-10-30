fbc7096c604aadb44347bb6d3cea283a42206b7f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2014-10-30 21:52:51 +0200

    update build scripts to find libotr in /usr/local/include if found

--- binding.gyp
+++ binding.gyp
@@ -4,6 +4,7 @@
       'target_name': 'otrnat',
       'sources': [ "src/otr.cc", "src/userstate.cc", "src/message.cc", "src/context.cc" ],
       'libraries': ['-lotr'],
+      'include_dirs': ['/usr/local/include'],
       'library_dirs': [
          '/usr/lib','/usr/local/lib'
       ],
--- build.sh
+++ build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 export otr_version=`./check-otr-version.sh`
 
-if [ "${otr_version}" == '' ]
-then
-    echo "No libotr found on the system."
-    echo "On Debian/Ubuntu you can install it with the following command:"
-    echo "sudo apt-get install libotr2 libotr2-dev"
-    exit
-fi
-
-echo "Found local version of libotr: ${otr_version}"
-
 if [ "${otr_version}" == '3' ]
 then
+    echo "Found local version of libotr: ${otr_version}"
     make module
+    exit
 fi
 
 if [ "${otr_version}" == '4' ]
 then
+    echo "Found local version of libotr: ${otr_version}"
     echo "module 'otr3' requires native libotr 3.2.1 on the system."
+    exit
 fi
+
+echo "No libotr found on the system."
+echo "On Debian/Ubuntu you can install it with the following command:"
+echo "sudo apt-get install libotr2 libotr2-dev"
+echo ""
+echo "On MacOS 10.x you can install it using brew:"
+echo "  brew install libotr"
 
